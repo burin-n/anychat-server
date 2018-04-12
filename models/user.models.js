d7352cf72ffe04fb76fd2aa5a81f373b7315243f
tmwtml <tangmo_wtml@hotmail.com>
2018-04-13 01:29:53 +0700

    user resgis & login

--- models/user.models.js
+++ models/user.models.js
@@ -2,6 +2,12 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let userSchema = Schema({
+	username: {
+		type: String,
+		require: true,
+		unique: true,
+		trim: true
+	},
 	name: String,
 	password: String,
 	chats: [Schema.Types.ObjectId]
